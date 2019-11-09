824c0a47b405248d4dc3302d21962efb71c05687
Vinicius Dias <carlosv775@gmail.com>
2019-11-09 12:24:47 -0300

    Video 3.2
    Criando método depositar e inicializando saldo com 0

--- src/Conta.php
+++ src/Conta.php
@@ -4,7 +4,7 @@ class Conta
 {
     public $cpfTitular;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
 
     public function sacar(float $valorASacar): void
     {
@@ -12,6 +12,15 @@ class Conta
             echo "Saldo indisponível";
         } else {
             $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+        } else {
+            $this->saldo += $valorADepositar;
         }
     }
 }
