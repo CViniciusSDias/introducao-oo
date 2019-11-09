b46e0c7679bd41966a700674ec0895e9d329a0ec
Vinicius Dias <carlosv775@gmail.com>
2019-11-09 16:13:37 -0300

    Video 6.2
    Extraindo variavel

--- banco.php
+++ banco.php
@@ -3,7 +3,8 @@
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 
-$primeiraConta = new Conta(new Titular('123.456.789-10', 'Vinicius Dias'));
+$vinicius = new Titular('123.456.789-10', 'Vinicius Dias');
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -11,7 +12,8 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta(new Titular('698.549.548-10', 'Patricia'));
+$patricia = new Titular('698.549.548-10', 'Patricia');
+$segundaConta = new Conta($patricia);
 var_dump($segundaConta);
 
 $outra = new Conta(new Titular('123', 'Abcdefg'));
